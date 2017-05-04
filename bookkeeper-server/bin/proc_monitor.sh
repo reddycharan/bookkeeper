7678db34ee9434178e097bdb11a4a2250f659de6
Dustin <dcastor@salesforce.com>
2017-05-04 16:20:18 -0700

    (@bug W-3936357@) Use symlink instead of searched directories.

--- bookkeeper-server/bin/proc_monitor.sh
+++ bookkeeper-server/bin/proc_monitor.sh
@@ -1,22 +1,20 @@
 #! /bin/bash
 
-MANIFEST_DIR=`ls -1tr ~/installed/ | grep -i sfstore__main | tail -n 1`
-CURRENT_STATUS=`cd ~/installed/$MANIFEST_DIR/bookkeeper/build; ./ant -q status`
+cd /home/sfstore/current/sfstorage/bookkeeper/build
+CURRENT_STATUS=`./ant -q status`
 #If sfstore is down, bring it back up; determined by its ./ant -q status check
 if [[ ${CURRENT_STATUS} == *"Not Running"* ]]; then
-    cd ~/installed/$MANIFEST_DIR/bookkeeper/build
-    #Attempt to stop, in the even it is somehow in hampered state and deemed not running
-    ./ant -q stop
+    #Starting implicitly stops both services without exiting.
     RESULT=`./ant -q start`
     if [[ $RESULT == *"BUILD SUCCESSFUL"* ]]; then
         echo "Successfully brought up bookkeeper via cron."
     else
-	echo "Failed to start bookie at `date`"
+        echo "Failed to start bookie at `date`"
     fi
 fi
 #If sfms is down, bring it back up; determined by its proc count of sfms (grep & actual proc)
 if [[ `ps -ef | grep -i [s]fms | wc -l` -lt 2 ]]; then
-    cd ~/installed/$MANIFEST_DIR/sayonara/build
+    cd /home/sfstore/current/sfstorage/sayonara/build
     ./ant sfms.startSfstoreMetrics
 fi
 
