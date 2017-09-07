239d0f4413ea95c79472dcf13519f0c5842d9efc
Dustin <dcastor@salesforce.com>
2017-09-07 11:01:38 -0700

    (@bug W-4240046@) Onboard sfstore repo into tnrp.

---
+++ docker/buildCmd.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+mvn clean install -DskipTests
+#Put it into the same structure as existing production directory.
+cp bookkeeper-server/target/bookkeeper-server-4.5.0-SNAPSHOT.jar bookkeeper-server/
+#CD to bookkeeper-server, and perform all operations relative to here.
+tar -C bookkeeper-server/ -cf sfstore.tar bookkeeper-server-4.5.0-SNAPSHOT.jar ../CHANGES.txt ../LICENSE ../NOTICE ../README bin/ conf/ lib/ certs/
+#Remove our previous copy.
+rm bookkeeper-server/bookkeeper-server-4.5.0-SNAPSHOT.jar
+#Zip it up & move it
+bzip2 -zfq ./sfstore.tar
+mv sfstore.tar.bz2 docker/
