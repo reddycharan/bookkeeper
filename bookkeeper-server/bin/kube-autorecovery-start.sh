0c532589480e6ac2e3ed315d874bd59a1a9f68a2
Dustin Castor <dcastor@salesforce.com>
2017-11-06 19:47:42 -0800

    (@bug W-4462466@) Make kube-common-start.sh executable & add environment variables. (#46)

--- bookkeeper-server/bin/kube-autorecovery-start.sh
+++ bookkeeper-server/bin/kube-autorecovery-start.sh
@@ -3,4 +3,7 @@
 #Override autorecovery-specific properties and then start-up autorecovery.
 
 bash ./kube-common-start.sh
+export BOOKIE_LOG_FILE_PATTERN=bookkeeper-autorecovery-$HOSTNAME-%d{yyyyMMddHH}.log
+export BOOKIE_LOG_FORMAT=$HOSTNAME.sfstore.autorecovery
+export BOOKIE_LOG_FILE=bookkeeper-autorecovery-$HOSTNAME.log
 /sfs/sfsbuild/bin/bookkeeper autorecovery
--- bookkeeper-server/bin/kube-bookkeeper-start.sh
+++ bookkeeper-server/bin/kube-bookkeeper-start.sh
@@ -3,6 +3,9 @@
 #Override bookkeeper-specific properties and then execute bookkeeper specific commands for initiating a new cluster in a k8s environment.
 
 bash ./kube-common-start.sh
+export BOOKIE_LOG_FILE_PATTERN=bookkeeper-bookie-$HOSTNAME-%d{yyyyMMddHH}.log
+export BOOKIE_LOG_FORMAT=$HOSTNAME.sfstore.bookie
+export BOOKIE_LOG_FILE=bookkeeper-bookie-$HOSTNAME.log
 /sfs/sfsbuild/bin/bookkeeper shell initnewcluster
 /sfs/sfsbuild/bin/bookkeeper shell bookieformat -d -nonInteractive
 /sfs/sfsbuild/bin/bookkeeper bookie
