ab067fda8c62746ec2ccb8a84f14b45df23f7b0b
Ankit Jain <jain.a@salesforce.com>
2018-03-16 15:26:42 -0700

    (@bug W-4240046@) Onboard sfstore repo into tnrp.
    [Adapted to 4.7]
    Matched docker folder contents to storage foundation expected manifests and
    base image paths to salesforce prd sources
    
    Signed-off-by: Ankit Jain <jain.a@salesforce.com>
    Signed-off-by: Samuel Just <sjust@salesforce.com>

---
+++ docker/buildCmd.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+mvn clean install -B -DskipTests
+#Put it into the same structure as existing production directory.
+cp bookkeeper-server/target/bookkeeper-server-4.7.0-SNAPSHOT.jar bookkeeper-server/
+#CD to bookkeeper-server, and perform all operations relative to here.
+tar -C bookkeeper-server/ -cf sfstore.tar bookkeeper-server-4.7.0-SNAPSHOT.jar ../LICENSE ../NOTICE ../README bin/ conf/ lib/ certs/
+#Remove our previous copy.
+rm bookkeeper-server/bookkeeper-server-4.7.0-SNAPSHOT.jar
+#Zip it up & move it
+gzip ./sfstore.tar
+mv sfstore.tar.gz docker/
