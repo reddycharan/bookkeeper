ff82d0e8dad27539b8f585371ef17c2f42933ff4
Ankit Jain <jain.a@salesforce.com>
2018-06-26 17:38:12 -0700

    (@W-4963336@) Adding mechanism to publish an eRelease build for fixes (#123)
    Modified script to check if the current release being targeted is a normal
    or an eRelease. In case of an eRelease, increment the patch version while
    retaining the older minor version. In case if weekly release, create a new 
    branch with the commits for the release and push the tag.
    
    (@rev kudayashankar@)
    (@rev sjust@)
    
    Signed-off-by: Ankit Jain <jain.a@salesforce.com>

--- sftools/bk_scripts/sfstore_version.sh
+++ sftools/bk_scripts/sfstore_version.sh
@@ -26,10 +26,19 @@ ARTIFACT_DIR=$WORKSPACE
 [[ -z "$IMAGE_NAME1" ]]      && echo "Error! \$IMAGE_NAME1 not set."  && exit -1
 [ -z "$TARGET_BRANCH" ] && echo "Error! \$TARGET_BRANCH not set." && exit -1
 [ -z "$UPDATE_GIT_TAGS" ] && echo "Error!  $UPDATE_GIT_TAGS was not set." && exit -1
+[ -z "$SFSTORE_ERELEASE" ] && echo "Error!  \$SFSTORE_ERELEASE was not set." && exit -1
 
 if [[ ! -f $ARTIFACT_DIR/$IMAGE_NAME1 ]];then
    echo "!!!ERROR: Artifacts were not copied from Build job. Please check"
    exit 1;
+fi
+
+# Check if the target_branch does exist
+echo "Checking if branch ${TARGET_BRANCH} exists..."
+git rev-parse --verify ${TARGET_BRANCH}
+if [ $? != 0 ];then
+  echo "ERROR! ${TARGET_BRANCH} is not present in git remote."
+  exit -1
 fi
 
 WORKSPACEIMAGEDIR=$WORKSPACE/tmpinstall
@@ -49,26 +58,56 @@ rm ${IMAGE_NAME1}
 
 # SFStore-Build jenkin job creates file : gitlog.info, containing the last git commit SHA, and tars it to $IMAGE_NAME1
 GIT_COMMIT=`cat gitlog.info`
-[ -z "$GIT_COMMIT" ]        && echo "Error! \$GIT_COMMIT not set, which means gitlog.info file is not created properly by SFStore-Build jenkin job."  && exit -1
+[ -z "$GIT_COMMIT" ] && echo "Error! \$GIT_COMMIT not set, which means gitlog.info file is not created properly by SFStore-Build jenkin job."  && exit -1
 echo "This new SFStore-Gold-Image is based on git commit SHA : $GIT_COMMIT"
 
 echo "sfstore.version.info file content:"
-cat ${SFSTORE_VERSION_INFO}
 SFSTORE_VERSION_INFO=${WORKSPACE}/sfstore.version.info
+cat ${SFSTORE_VERSION_INFO}
 SFSTORE_MAJOR_VERSION=`grep "^SFSTORE_MAJOR_VERSION=" ${SFSTORE_VERSION_INFO} | awk -F"=" '{ print $2 }'`
 echo "major=${SFSTORE_MAJOR_VERSION}"
 SFSTORE_MINOR_VERSION=`grep "^SFSTORE_MINOR_VERSION=" ${SFSTORE_VERSION_INFO} | awk -F"=" '{ print $2 }'`
 echo "minor=${SFSTORE_MINOR_VERSION}"
 SFSTORE_PATCH_VERSION=`grep "^SFSTORE_PATCH_VERSION=" ${SFSTORE_VERSION_INFO} | awk -F"=" '{ print $2 }'`
 echo "patch=${SFSTORE_PATCH_VERSION}"
+CURR_VERSION_STRING=${SFSTORE_MAJOR_VERSION}.${SFSTORE_MINOR_VERSION}.${SFSTORE_PATCH_VERSION}
 
-NEW_VERSION_STRING=${SFSTORE_MAJOR_VERSION}.${SFSTORE_MINOR_VERSION}.${SFSTORE_PATCH_VERSION}
-echo "New version string will be $NEW_VERSION_STRING"
+# If this is not an eRelease, create a new branch for the release
+if [ "x$SFSTORE_ERELEASE" != "xtrue" ];then
+  RELEASE_BRANCH="SFSTORE_${SFSTORE_MAJOR_VERSION}.${SFSTORE_MINOR_VERSION}"
+  echo "RELEASE_BRANCH=${RELEASE_BRANCH}"
+
+  echo "Testing to see if branch ${RELEASE_BRANCH} already exists..."
+  if [ ! -z $(git rev-parse --verify ${RELEASE_BRANCH} 2> /dev/null) ]; then
+    echo "ERROR! Branch ${RELEASE_BRANCH} already exists!"
+    exit -1
+  fi
+
+  echo "Creating new branch..."
+  git checkout -b ${RELEASE_BRANCH} ${GIT_COMMIT}
+  if [ ! "$(git rev-parse --abbrev-ref HEAD)" == "${RELEASE_BRANCH}" ];then
+    echo "ERROR! Couldn't create local branch ${RELEASE_BRANCH}."
+    exit -1
+  fi
+
+  git push -u origin ${RELEASE_BRANCH}
+
+  BRANCH_EXISTS=$(git ls-remote --heads origin ${RELEASE_BRANCH})
+  if [ -z "${BRANCH_EXISTS}" ];then
+    echo -e "ERROR! Branch ${RELEASE_BRANCH} not created successfully."
+    exit -1
+  else
+    echo -e "\n\n Branch ${RELEASE_BRANCH} Created and pushed to remote successfully \n\n"
+  fi
+fi
+
+# Switch to initial target_branch
+git checkout ${TARGET_BRANCH}
 
 # rename the jar files and repackage it with the right version string
 cd $DEPLOY_SFSTORE
-mv bookkeeper-server*.jar bookkeeper-server-${NEW_VERSION_STRING}.jar
-NEW_SFSTORE_DEPLOY_NAME=deploy-sfstore-${NEW_VERSION_STRING}.tar
+mv bookkeeper-server*.jar bookkeeper-server-${CURR_VERSION_STRING}.jar
+NEW_SFSTORE_DEPLOY_NAME=deploy-sfstore-${CURR_VERSION_STRING}.tar
 tar cf ${ARTIFACT_DIR}/${NEW_SFSTORE_DEPLOY_NAME} *
 bzip2 -zfq ${ARTIFACT_DIR}/${NEW_SFSTORE_DEPLOY_NAME}
 
@@ -76,16 +115,64 @@ bzip2 -zfq ${ARTIFACT_DIR}/${NEW_SFSTORE_DEPLOY_NAME}
 if [ "x$UPDATE_GIT_TAGS" = "xtrue" ];then
   cd $WORKSPACE
   echo "Tagging the version number for sfstore"
-  GIT_GOLD_IMAGE_TAG=Gold_Image_${NEW_VERSION_STRING}
+  GIT_GOLD_IMAGE_TAG=Gold_Image_${CURR_VERSION_STRING}
   git tag $GIT_GOLD_IMAGE_TAG $GIT_COMMIT
-  # Increment SFSTORE_MINOR_VERSION version number
+
+  SFSTORE_PATCH_VERSION_PLUS_ONE=$((SFSTORE_PATCH_VERSION+1))
   SFSTORE_MINOR_VERSION_PLUS_ONE=$((SFSTORE_MINOR_VERSION+1))
-  sed -i -E "s/SFSTORE_MINOR_VERSION=$SFSTORE_MINOR_VERSION/SFSTORE_MINOR_VERSION=$SFSTORE_MINOR_VERSION_PLUS_ONE/" sfstore.version.info
-  cat "New version of sfstore.version.info to be checked in to repo is:"
-  cat sfstore.version.info
-  git add sfstore.version.info
-  git commit -m "(@bug W-3302860@) Tag $GIT_GOLD_IMAGE_TAG and update SFSTORE_MINOR_VERSION to $SFSTORE_MINOR_VERSION_PLUS_ONE"
-  git push origin $TARGET_BRANCH
+  # Check if eRelease in which case update the patch version while retaining the previous minor version
+  if [ "x$SFSTORE_ERELEASE" = "xtrue" ];then
+  NEW_VERSION_STRING=${SFSTORE_MAJOR_VERSION}.${SFSTORE_MINOR_VERSION}.${SFSTORE_PATCH_VERSION_PLUS_ONE}
+  else
+  NEW_VERSION_STRING=${SFSTORE_MAJOR_VERSION}.${SFSTORE_MINOR_VERSION_PLUS_ONE}.${SFSTORE_PATCH_VERSION}
+  fi
+
+  if [ "x$SFSTORE_ERELEASE" = "xtrue" ];then
+    # Update only the target branch in case of eRelease
+
+    # Checkout the target branch(in this case SFSTORE_(MAJOR).(MINOR) ). Update the patch version in the release branch. Commit and push the change to the repo. Show the new sfstore.version.info
+    git checkout ${TARGET_BRANCH}
+    sed -i -E "s/SFSTORE_PATCH_VERSION=$SFSTORE_PATCH_VERSION/SFSTORE_PATCH_VERSION=$SFSTORE_PATCH_VERSION_PLUS_ONE/" sfstore.version.info
+    git add sfstore.version.info
+    git commit -m "(@bug W-3302860@) Tag $GIT_GOLD_IMAGE_TAG and update SFSTORE_PATCH_VERSION to $SFSTORE_PATCH_VERSION_PLUS_ONE"
+    if $(git push origin $TARGET_BRANCH)
+    then
+    echo "Successfully pushed the updated version number to the target branch"
+    else
+    echo "Push to the target branch failed. Please check"
+     fi
+  else
+    # Update the target branch(sfstore) as well as the release branch created in case of normal release
+
+    # Checkout the target branch(in this case sfstore). Update the minor version in the sfstore branch. Commit and push the change to the repo. Show the new sfstore.version.info
+    git checkout ${TARGET_BRANCH}
+    sed -i -E "s/SFSTORE_MINOR_VERSION=$SFSTORE_MINOR_VERSION/SFSTORE_MINOR_VERSION=$SFSTORE_MINOR_VERSION_PLUS_ONE/" sfstore.version.info
+    git add sfstore.version.info
+    git commit -m "(@bug W-3302860@) Tag $GIT_GOLD_IMAGE_TAG and update SFSTORE_MINOR_VERSION to $SFSTORE_MINOR_VERSION_PLUS_ONE"
+    if $(git push origin $TARGET_BRANCH)
+    then
+    echo "Successfully pushed the updated version number to the target branch"
+    else
+    echo "Push to the target branch failed. Please check"
+     fi
+    echo "New version of sfstore.version.info checked in to repo in branch ${TARGET_BRANCH} is:"
+    cat sfstore.version.info
+
+    # Checkout the target branch(in this case SFSTORE_(MAJOR).(MINOR) ). Update the patch version in the release branch. Commit and push the change to the repo. Show the new sfstore.version.info
+    git checkout ${RELEASE_BRANCH}
+    sed -i -E "s/SFSTORE_PATCH_VERSION=$SFSTORE_PATCH_VERSION/SFSTORE_PATCH_VERSION=$SFSTORE_PATCH_VERSION_PLUS_ONE/" sfstore.version.info
+    git add sfstore.version.info
+    git commit -m "(@bug W-3302860@) Tag $GIT_GOLD_IMAGE_TAG and update SFSTORE_PATCH_VERSION to $SFSTORE_PATCH_VERSION_PLUS_ONE"
+    if $(git push origin $RELEASE_BRANCH)
+    then
+    echo "Successfully pushed the updated version number to the release branch"
+    else
+    echo "Push to the release branch failed. Please check"
+     fi
+    echo "New version of sfstore.version.info checked in to repo in branch ${RELEASE_BRANCH} is:"
+    cat sfstore.version.info
+  fi
+
   git push --tags
 else
   echo "You chose not to update git tags.."
