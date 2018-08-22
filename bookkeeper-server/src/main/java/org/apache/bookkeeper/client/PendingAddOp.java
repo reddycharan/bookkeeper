cc3cda65fb270ad2be0fdedf89e6bed83713d3ce
Andrey Yegorov <ayegorov@salesforce.com>
2018-08-21 20:45:22 -0700

    (@bug W-5366749@) Issue #1584: LedgerHandleAdv should expose asyncAddEntry variant that takes ByteBuf (LedgerHandle exposes it as public) (#151)
    Cherry-pick of the community fix.
    The only difference from community change is that I dropped bookkeeper-server/src/test/java/org/apache/bookkeeper/client/DeferredSyncTest.java
    It is a part of another change that we do not need right now.
    
    Descriptions of the changes in this PR:
    
    - exposed asyncAddEntry as public, similarly to other variants.
    - fixed ByteBuf retention
    
    It's useful to have this exposed as public for clients to make use of netty's allocator and pass ByteBuf directly.
    
    exposed asyncAddEntry as public, similarly to other variants.
    
    Master Issue: #1584
    
    Author: Andrey Yegorov <ayegorov@salesforce.com>
    @rev vjujjuri@
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>, Sijie Guo <sijie@apache.org>
    
    This closes #1585 from dlg99/feature/issue-1584-LedgerHandleAdv-public-addEntry-bytebuff, closes #1584

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/PendingAddOp.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/PendingAddOp.java
@@ -240,6 +240,8 @@ class PendingAddOp extends SafeRunnable implements WriteCallback {
         this.toSend = lh.macManager.computeDigestAndPackageForSending(
                 entryId, lh.lastAddConfirmed, currentLedgerLength,
                 payload);
+        // ownership of RefCounted ByteBuf was passed to computeDigestAndPackageForSending
+        payload = null;
 
         // We are about to send. Check if we need to make an ensemble change
         // becasue of delayed write errors
@@ -448,7 +450,10 @@ class PendingAddOp extends SafeRunnable implements WriteCallback {
     private void recyclePendAddOpObject() {
         entryId = LedgerHandle.INVALID_ENTRY_ID;
         currentLedgerLength = -1;
-        payload = null;
+        if (payload != null) {
+            ReferenceCountUtil.release(payload);
+            payload = null;
+        }
         cb = null;
         ctx = null;
         ackSet.recycle();
