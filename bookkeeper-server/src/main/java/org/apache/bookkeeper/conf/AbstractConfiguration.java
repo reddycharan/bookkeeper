1e7f4bb09656ecbaaa07819bd0733f751b0d41de
cguttapalem <cguttapalem@salesforce.com>
2018-09-20 18:47:03 -0700

    (@bug W-5278287@) Enable storeSystemTimeAsLedgerUnderreplicatedMarkTime
    - Enable the Auditor to use system time as underreplicated ledger mark time.
    If this is enabled, Auditor will write a ctime field into the
    underreplicated ledger znode.
    
    @rev sjust@

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/conf/AbstractConfiguration.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/conf/AbstractConfiguration.java
@@ -951,7 +951,7 @@ public abstract class AbstractConfiguration<T extends AbstractConfiguration>
      *         underreplicated ledger mark time.
      */
     public boolean getStoreSystemTimeAsLedgerUnderreplicatedMarkTime() {
-        return getBoolean(STORE_SYSTEMTIME_AS_LEDGER_UNDERREPLICATED_MARK_TIME, false);
+        return getBoolean(STORE_SYSTEMTIME_AS_LEDGER_UNDERREPLICATED_MARK_TIME, true);
     }
 
     /**
