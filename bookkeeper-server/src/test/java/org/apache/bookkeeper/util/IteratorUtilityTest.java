7c5eb0171c3f65f9127895bcb23e888f3f08aa47
Charan Reddy Guttapalem <cguttapalem@salesforce.com>
2019-04-10 12:06:08 -0700

    iteratorutilitytest

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/util/IteratorUtilityTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/util/IteratorUtilityTest.java
@@ -22,6 +22,7 @@ package org.apache.bookkeeper.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.PrimitiveIterator;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
@@ -35,16 +36,19 @@ public class IteratorUtilityTest {
     public void testWithPrimitiveItrMerge() {
         long[][] arrays = { 
                 { 0, 1, 2 }, 
-                { 1, 2 }, 
+                { 0, 1 }, 
                 { 1, 2 },
                 { 1, 2, 3, 5, 6, 7, 8 }, 
+                { 1, 2, 3, 5, 6, 7, 8 },
                 { 0, 1, 5 }, 
                 { 3 }, 
                 { 1, 2, 4, 5, 7, 8 },
                 {},
                 {},
                 { 0 }, 
-                { 1, 2, 3, 5, 6, 11, 12, 13, 14, 15, 16, 17, 100, 1000, 1001, 10000, 20000, 20001 }
+                { 1, 2, 3, 5, 6, 11, 12, 13, 14, 15, 16, 17, 100, 1000, 1001, 10000, 20000, 20001 },
+                { 201, 202, 203, 205, 206, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 20100, 201000,
+                    201001, 2010000, 2020000, 2020001 }
         };
         for (int i = 0; i < arrays.length; i++) {
             for (int j = i + 1; j < arrays.length; j++) {
@@ -63,6 +67,58 @@ public class IteratorUtilityTest {
 
                 PrimitiveIterator.OfLong mergedItr = IteratorUtility.mergePrimitiveLongIterator(primitiveIterator1,
                         primitiveIterator2);
+                ArrayList<Long> mergedArrayList = new ArrayList<Long>();
+                Consumer<Long> addMethod = mergedArrayList::add;
+                mergedItr.forEachRemaining(addMethod);
+                int mergedListSize = mergedArrayList.size();
+                Assert.assertEquals("Size of the mergedArrayList", unionSet.size(), mergedArrayList.size());
+                Assert.assertTrue("mergedArrayList should contain all elements in unionSet",
+                        mergedArrayList.containsAll(unionSet));
+                Assert.assertTrue("Merged Iterator should be sorted", IntStream.range(0, mergedListSize - 1)
+                        .allMatch(k -> mergedArrayList.get(k) <= mergedArrayList.get(k + 1)));
+                Assert.assertTrue("All elements of tempArray1 should be in mergedArrayList",
+                        IntStream.range(0, tempArray1.length).allMatch(k -> mergedArrayList.contains(tempArray1[k])));
+                Assert.assertTrue("All elements of tempArray2 should be in mergedArrayList",
+                        IntStream.range(0, tempArray2.length).allMatch(k -> mergedArrayList.contains(tempArray2[k])));
+            }
+        }
+    }
+    
+    @Test
+    public void testWithItrMerge() {
+        long[][] arrays = { 
+                { 0, 1, 2 }, 
+                { 0, 1 }, 
+                { 1, 2 },
+                { 1, 2, 3, 5, 6, 7, 8 }, 
+                { 1, 2, 3, 5, 6, 7, 8 },
+                { 0, 1, 5 }, 
+                { 3 }, 
+                { 1, 2, 4, 5, 7, 8 },
+                {},
+                {},
+                { 0 }, 
+                { 1, 2, 3, 5, 6, 11, 12, 13, 14, 15, 16, 17, 100, 1000, 1001, 10000, 20000, 20001 },
+                { 201, 202, 203, 205, 206, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 20100, 201000,
+                    201001, 2010000, 2020000, 2020001 }
+        };
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = i + 1; j < arrays.length; j++) {
+                long[] tempArray1 = arrays[i];
+                ArrayList<Long> tempArrayList1 = new ArrayList<Long>();
+                IntStream.range(0, tempArray1.length).forEach((k) -> tempArrayList1.add(tempArray1[k]));
+                long[] tempArray2 = arrays[j];
+                ArrayList<Long> tempArrayList2 = new ArrayList<Long>();
+                IntStream.range(0, tempArray2.length).forEach((k) -> tempArrayList2.add(tempArray2[k]));
+                HashSet<Long> unionSet = new HashSet<Long>();
+                unionSet.addAll(tempArrayList1);
+                unionSet.addAll(tempArrayList2);
+
+                Iterator<Long> itr1 = tempArrayList1.iterator();
+                Iterator<Long> itr2 = tempArrayList2.iterator();
+
+                Iterator<Long> mergedItr = IteratorUtility.mergeIteratorsForPrimitiveLongIterator(itr1, itr2,
+                        Long::compare, (l) -> l);
                 ArrayList<Long> mergedArrayList = new ArrayList<Long>();
                 Consumer<Long> addMethod = mergedArrayList::add;
                 mergedItr.forEachRemaining(addMethod);
